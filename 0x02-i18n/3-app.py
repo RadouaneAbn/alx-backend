8a6da84392340a82ea906b61f4a58d84f2dd0920
RadouaneAbn <radouaneabn@gmail.com>
2024-07-31 02:08:56 +0100

    Task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ This module is flask app that serves one route '/' """
 from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+from flask_babel import Babel
 from typing import Any, List
 
 
@@ -29,7 +29,7 @@ def get_locale() -> str:
 @app.route("/", methods=["GET"])
 def root() -> Any:
     """ This is the root/home page """
-    return render_template("2-index.html")
+    return render_template("3-index.html")
 
 
 if __name__ == '__main__':
