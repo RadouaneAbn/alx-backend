7d5bb0a188c0906407afa6232efec42fd971b8b0
RadouaneAbn <radouaneabn@gmail.com>
2024-07-31 01:41:46 +0100

    move to sandbox

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" This module is flask app that serves one route '/' """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext
+from typing import Any, List
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """ The configuration class for the Flask app """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app.config.from_object(Config)
+babel: Babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """ Select the best match locale based on the Accept-Language header """
+    return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+@app.route("/", methods=["GET"])
+def root() -> Any:
+    """ This is the root/home page """
+    return render_template("2-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
