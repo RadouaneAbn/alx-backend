a013e958f18af229d9d06752781da3c0c681853e
RadouaneAbn <radouaneabn@gmail.com>
2024-07-31 01:28:09 +0100

    Task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" This module is flask app that serves one route '/' """
+from flask import Flask, render_template
+from flask_babel import Babel
+from typing import Any, List
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+class Config:
+    """ The configuration class for the Flask app """
+    LANGUAGES: List[str] = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE: str = "en"
+    BABEL_DEFAULT_TIMEZONE: str = "UTC"
+
+
+app.config.from_object(Config)
+babel: Babel = Babel(app)
+
+
+@app.route("/", methods=["GET"])
+def root() -> Any:
+    """ This is the root/home page """
+    return render_template("0-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
