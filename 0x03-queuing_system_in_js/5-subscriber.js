2d185b02aec0632815fedc2790b693096f8122d3
RadouaneAbn <radouaneabn@gmail.com>
2024-08-28 21:26:10 +0100

    Task 5

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import { createClient, print } from "redis";
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`)
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, msg) => {
+  console.log(msg);
+  if (msg === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+})
